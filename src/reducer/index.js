0fdf8b4d5040616cda66db3fb482858657ba2b83
gaurav bisht <gauravengg.bisht@gmail.com>
2024-05-02 10:23:33 +0530

    CourseSection issue resolve

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -3,12 +3,14 @@ import authReducer from "../slices/authSlice";
 import profileReducer from "../slices/profileSlice";
 import cartReducer from "../slices/cartSlice";
 import courseReducer from "../slices/courseSlice"
+import viewCourseSliceReducer from "../slices/viewCourseSlice";
 
 const rootReducer = combineReducers ({
     auth: authReducer,
     profile:profileReducer,
     cart:cartReducer,
     course:courseReducer,
+    viewCourse:viewCourseSliceReducer
 })
 
 export default rootReducer;
